d960a801bdab2b51d012a83236ae4103938085e8
open_mailbox <dev@open-mailbox.com>
2022-07-02 19:35:07 -0400

    Add an example Lua resource.

---
+++ src/[examples]/example-lua/fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version "cerulean"
+games { "gta5" }
+lua54 "yes"
+
+author "open_mailbox <dev@open-mailbox.com>"
+description "Change this description."
+version "0.0.1"
+
+dependencies {
+    --"mysql-async",
+}
+
+client_scripts {
+    "shared/**/*.lua",
+    "client/**/*.lua"
+}
+
+server_scripts {
+    --"@mysql-async/lib/MySQL.lua",
+    "shared/**/*.lua",
+    "server/**/*.lua"
+}
+
+--ui_page "web/index.html"
+--
+--files {
+--    "web/**/*.html",
+--    "web/**/*.js",
+--    "web/**/*.css",
+--}
+
+-- files {
+--}
+
+--exports {
+--}
+
+--server_exports {
+--}
+
+-- server_only 'yes'
