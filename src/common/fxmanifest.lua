e30b48811ceecd88f964502fc5607d42983feabb
open_mailbox <dev@open-mailbox.com>
2022-07-03 11:10:24 -0400

    Add warnings if someone tries to start the common resource.

--- src/common/fxmanifest.lua
+++ src/common/fxmanifest.lua
@@ -11,3 +11,11 @@ version "0.0.1"
 shared_scripts {
     "shared/**/*.lua",
 }
+
+client_scripts {
+    "client/**/*.lua"
+}
+
+server_scripts {
+    "server/**/*.lua"
+}
--- src/common/shared/events.lua
+++ src/common/shared/events.lua
@@ -1,7 +1,10 @@
 Events = {}
 
 -- Base FiveM things
-Events.ON_RESOURCE_START = "onResourceStart"
+Events.ON_RESOURCE_START        = "onResourceStart"
+Events.ON_RESOURCE_STOP         = "onResourceStop"
+Events.ON_CLIENT_RESOURCE_START = "onClientResourceStart"
+Events.ON_CLIENT_RESOURCE_STOP  = "onClientResourceStop"
 
 -- @resource chat
 Events.ADD_CHAT_MESSAGE = "chat:addMessage"
