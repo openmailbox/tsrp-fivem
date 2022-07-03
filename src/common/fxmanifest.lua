959097f3f18a38e160108aab5d7b83ace1dce70e
open_mailbox <dev@open-mailbox.com>
2022-07-03 11:04:44 -0400

    Add common resource for shared definitions.

---
+++ src/common/fxmanifest.lua
@@ -0,0 +1,13 @@
+---@diagnostic disable: undefined-global
+
+fx_version "cerulean"
+games { "gta5" }
+lua54 "yes"
+
+author "open_mailbox <dev@open-mailbox.com>"
+description "Provides common global definitions meant to be included in other resources."
+version "0.0.1"
+
+shared_scripts {
+    "shared/**/*.lua",
+}
---
+++ src/common/shared/events.lua
@@ -0,0 +1,7 @@
+Events = {}
+
+-- Base FiveM things
+Events.ON_RESOURCE_START = "onResourceStart"
+
+-- @resource chat
+Events.ADD_CHAT_MESSAGE = "chat:addMessage"
