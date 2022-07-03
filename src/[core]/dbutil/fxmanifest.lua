6c5a5d10f8b045ee98a0f47eade9842f91981bd6
open_mailbox <dev@open-mailbox.com>
2022-07-03 10:08:59 -0400

    Add initial template files for dbutil. Write a brief readme.

---
+++ src/[core]/dbutil/fxmanifest.lua
@@ -0,0 +1,43 @@
+---@diagnostic disable: undefined-global
+
+fx_version "cerulean"
+games { "gta5" }
+lua54 "yes"
+
+author "open_mailbox <dev@open-mailbox.com>"
+description "This resource does nothing. It's just a template for getting started."
+version "0.0.1"
+
+dependencies {
+    --"mysql-async",
+}
+
+client_scripts {
+    "shared/**/*.lua",
+    "client/**/*.lua"
+}
+
+server_scripts {
+    --"@mysql-async/lib/MySQL.lua",
+    "shared/**/*.lua",
+    "server/**/*.lua"
+}
+
+--ui_page "web/index.html"
+--
+--files {
+--    "web/**/*.html",
+--    "web/**/*.js",
+--    "web/**/*.css",
+--}
+
+-- files {
+--}
+
+--exports {
+--}
+
+--server_exports {
+--}
+
+-- server_only 'yes'
