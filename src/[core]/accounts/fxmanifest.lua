897190e64e16da833883494b58f75eeb673603f4
open_mailbox <dev@open-mailbox.com>
2022-07-04 12:34:20 -0400

    Add migrations for accounts and identifiers tables.

--- src/[core]/accounts/fxmanifest.lua
+++ src/[core]/accounts/fxmanifest.lua
@@ -5,7 +5,7 @@ games { "gta5" }
 lua54 "yes"
 
 author "open_mailbox <dev@open-mailbox.com>"
-description "Handles player account management in a platform-agnostic way."
+description "Handles the queue and overall player account management in a platform-agnostic way."
 version "0.0.1"
 
 dependencies {
