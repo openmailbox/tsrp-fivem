2a4679b75d80f481c10324de7cde210c3547ed6b
open_mailbox <dev@open-mailbox.com>
2022-07-22 21:13:03 -0400

    Wire up session lifecycle for ATMs.

---
+++ src/atm/web/scripts/deposit.js
@@ -0,0 +1,37 @@
+import { createApp } from 'vue'
+
+Atm.Deposit = (function() {
+    let vm = null;
+
+    const app = createApp({
+        data() {
+            return {
+                isActive: false
+            }
+        },
+        methods: {
+            cancel() {
+                fetch("https://atm/atm:DeleteSession", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json; charset=UTF-8" }
+                });
+
+                isActive = false;
+            },
+
+            createSession() {
+                isActive = true;
+            },
+        }
+    });
+
+    const createSession = function() {
+        vm.createSession();
+    }
+
+    vm = app.mount('#atm');
+
+    return {
+        createSession: createSession
+    }
+})();
