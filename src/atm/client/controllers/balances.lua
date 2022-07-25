e80998516ac9e4ec5a281fba5950ab31d5c52daa
open_mailbox <dev@open-mailbox.com>
2022-07-24 21:55:21 -0400

    Remove stray debug print.

--- src/atm/client/controllers/balances.lua
+++ src/atm/client/controllers/balances.lua
@@ -1,5 +1,4 @@
 local function update(data)
-    print("update balance to " .. data.new_balance)
     StatSetInt(GetHashKey("BANK_BALANCE"), data.new_balance, true)
 end
 RegisterNetEvent(Events.UPDATE_BANK_BALANCE, update)
