418c02d01ee06fc9ac36c1afc721a1fe60f56ce3
open_mailbox <dev@open-mailbox.com>
2022-07-25 22:23:20 -0400

    Add common object definitions.

---
+++ src/common/shared/objects.lua
@@ -0,0 +1,41 @@
+Objects = {}
+
+Objects.DUMPSTERS = {
+    GetHashKey("prop_cs_dumpster_01a"),
+    GetHashKey("p_dumpster_t"),
+    GetHashKey("prop_snow_dumpster_01"),
+    GetHashKey("prop_dumpster_01a"),
+    GetHashKey("prop_dumpster_02a"),
+    GetHashKey("prop_dumpster_02b"),
+    GetHashKey("prop_dumpster_3a"),
+    GetHashKey("prop_dumpster_4a"),
+    GetHashKey("prop_dumpster_4b"),
+}
+
+Objects.TRASH_BINS = {
+    GetHashKey("prop_bin_01a"),
+    GetHashKey("prop_bin_02a"),
+    GetHashKey("prop_bin_03a"),
+    GetHashKey("prop_bin_04a"),
+    GetHashKey("prop_bin_05a"),
+    GetHashKey("prop_bin_06a"),
+    GetHashKey("prop_bin_07a"),
+    GetHashKey("prop_bin_07b"),
+    GetHashKey("prop_bin_07c"),
+    GetHashKey("prop_bin_07d"),
+    GetHashKey("prop_bin_08a"),
+    GetHashKey("prop_bin_08open"),
+    GetHashKey("prop_bin_09a"),
+    GetHashKey("prop_bin_10a"),
+    GetHashKey("prop_bin_10b"),
+    GetHashKey("prop_bin_11a"),
+    GetHashKey("prop_bin_11b"),
+    GetHashKey("prop_bin_12a"),
+    GetHashKey("prop_bin_14a"),
+    GetHashKey("prop_bin_14b"),
+    GetHashKey("prop_bin_beach_01a"),
+    GetHashKey("prop_bin_beach_01d"),
+    GetHashKey("prop_bin_delpiero"),
+    GetHashKey("prop_bin_delpiero_b"),
+    GetHashKey("zprop_bin_01a_old"),
+}
