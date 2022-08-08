99ca0df0858f2524b9b7c95ae3dd6e3025e2f364
open_mailbox <dev@open-mailbox.com>
2022-08-07 21:45:37 -0400

    Experiment with camera controls during a weapon modding session.

--- src/weaponsmith/client/controllers/weaponsmith_sessions.lua
+++ src/weaponsmith/client/controllers/weaponsmith_sessions.lua
@@ -3,8 +3,41 @@ local function create()
         return false
     end
 
-    if GetCurrentPedWeapon(PlayerPedId(), Weapons.UNARMED, 1) then
+    local succ, weapon = GetCurrentPedWeapon(PlayerPedId(), 1)
+
+    if not succ or weapon == Weapons.UNARMED then
         return false
     end
+
+    local ploc   = GetEntityCoords(PlayerPedId())
+    local spot   = ploc + GetEntityForwardVector(PlayerPedId()) * 1.5
+    local object = CreateWeaponObject(weapon, 0, spot.x, spot.y, spot.z, true, 1.0, 0, 0, 0)
+    local cloc   = spot + GetEntityForwardVector(object)
+    local camera = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", cloc.x, cloc.y, cloc.z, 0, 0, 0, 60.0, false, 0)
+
+    SetCamUseShallowDofMode(camera, true)
+    SetCamNearDof(camera, 0.3)
+    SetCamFarDof(camera, 1.0)
+    SetCamDofStrength(camera, 1.0)
+    PointCamAtEntity(camera, object, 0, 0, 0, 1)
+    SetCamActive(camera, true)
+    RenderScriptCams(true, true, 1000, true, true)
+
+    Citizen.CreateThread(function()
+        while true do
+            DisableControlAction(0, 177, true)
+            SetUseHiDof()
+
+            if IsDisabledControlJustPressed(0, 177) then
+                break
+            end
+
+            Citizen.Wait(0)
+        end
+
+        SetCamActive(camera, false)
+        RenderScriptCams(false, true, 1000, true, true)
+        DeleteObject(object)
+    end)
 end
 RegisterNetEvent(Events.CREATE_WEAPONSMITH_SESSION, create)
--- src/weaponsmith/fxmanifest.lua
+++ src/weaponsmith/fxmanifest.lua
@@ -9,10 +9,12 @@ description "Provides an interface for modifying/managing weapon attachments."
 version "0.0.1"
 
 client_scripts {
+    "@common/shared/events.lua",
     "@common/shared/weapons.lua",
     "client/**/*.lua"
 }
 
 server_scripts {
+    "@common/shared/events.lua",
     "server/**/*.lua"
 }
