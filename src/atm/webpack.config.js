76a7315ce14523dbac8c062c1c2836e2329ca9c7
open_mailbox <dev@open-mailbox.com>
2022-08-13 21:33:25 -0400

    Get Vue app to successfully initialize via webpack build.

--- src/atm/webpack.config.js
+++ src/atm/webpack.config.js
@@ -1,16 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    deposit: './web/src/scripts/deposit.js',
-    events: './web/src/scripts/events.js',
+    main: './web/src/scripts/index.js',
+    events: './web/src/scripts/events.js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: './web/src/index.html'
     })
   ],
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, './web/dist'),
@@ -18,10 +26,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          'vue-style-loader',
+          'css-loader',
         ]
       }
     ]
