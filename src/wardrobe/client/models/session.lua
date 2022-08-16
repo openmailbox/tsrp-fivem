0ddf476b8ea43fd2d9aaa29d99450f092533b608
open_mailbox <dev@open-mailbox.com>
2022-08-15 20:41:43 -0400

    Enable camera high DoF during wardrobe. Poll for armor/health/position changes and break session.

--- src/wardrobe/client/models/session.lua
+++ src/wardrobe/client/models/session.lua
@@ -22,6 +22,8 @@ function Session:new(o)
 end
 
 function Session:finish()
+    active_session = nil
+
     local _, forward = GetCamMatrix(self.camera)
     local target     = GetCamCoord(self.camera) + (5 * forward)
 
@@ -33,8 +35,6 @@ function Session:finish()
     if not self.hide_radar then
         DisplayRadar(true)
     end
-
-    active_session = nil
 
     Citizen.Wait(1000)
     ClearPedTasks(PlayerPedId())
@@ -90,10 +90,30 @@ end
 -- @local
 function start_session(session)
     Citizen.CreateThread(function()
+        local ped             = PlayerPedId()
+        local starting_armor  = GetPedArmour(ped)
+        local starting_health = GetEntityHealth(ped)
+        local starting_xyz    = GetEntityCoords(ped)
+        local next_check      = GetGameTimer() + 1000
+
+        local new_armor, new_health, new_xyz
+
         while session.active do
             SetUseHiDof() -- enables camera depth of field
 
-            -- TODO: Poll for health/armor changes and position changes, and kill session if detected.
+            if GetGameTimer() > next_check then
+                new_armor  = GetPedArmour(ped)
+                new_health = GetEntityHealth(ped)
+                new_xyz    = GetEntityCoords(ped)
+                next_check = GetGameTimer() + 1000
+
+                if Vdist(starting_xyz, new_xyz) > 1.0 or
+                    new_armor < starting_armor or
+                    new_health < starting_health then
+
+                    active_session:finish()
+                end
+            end
 
             Citizen.Wait(0)
         end
