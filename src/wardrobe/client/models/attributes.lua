393e89950c63b342c8e631c01b588cb9d15ada9d
open_mailbox <dev@open-mailbox.com>
2022-08-21 14:07:30 -0400

    Add component serialization for populating the wardrobe UI.

---
+++ src/wardrobe/client/models/attributes.lua
@@ -0,0 +1,17 @@
+AttributeTypes = {
+    COMPONENT    = 1,
+    FACE_FEATURE = 2,
+    EYE_COLOR    = 3,
+    HAIR_COLOR   = 4,
+    HEAD_BLEND   = 5,
+    HEAD_OVERLAY = 6,
+    MAKEUP_COLOR = 7,
+    PROP         = 8,
+    TATTOO       = 9
+}
+
+Attributes = {
+    ["face"]  = { label = "Face",  type = AttributeTypes.COMPONENT, index = 0 },
+    ["hair"]  = { label = "Hair",  type = AttributeTypes.COMPONENT, index = 2 },
+    ["shoes"] = { label = "Shoes", type = AttributeTypes.COMPONENT, index = 6 }
+}
--- src/wardrobe/client/web_serializer.lua
+++ src/wardrobe/client/web_serializer.lua
@@ -1,5 +1,8 @@
 WebSerializer = {}
 
+-- Forward declarations
+local serialize_component
+
 function WebSerializer:new(o)
     o = o or {}
 
@@ -9,8 +12,60 @@ function WebSerializer:new(o)
     return o
 end
 
--- Serialize a ped into a format that can be used by the web UI.
+-- Serialize the ped into a format that can be used to initialize the web UI.
 function WebSerializer:serialize()
+    local attributes = {}
+
+    for name, details in pairs(Attributes) do
+        if details.type == AttributeTypes.COMPONENT then
+            table.insert(attributes, serialize_component(self.ped, name))
+        end
+    end
+
     return {
+        categories = attributes
+    }
+end
+
+-- @local
+function serialize_component(ped, name)
+    local attribute   = Attributes[name]
+    local draw_count  = GetNumberOfPedDrawableVariations(ped)
+    local texture_map = {}
+
+    for i = 1, draw_count do
+        local texture_count = GetNumberOfPedTextureVariations(ped, attribute.index, i - 1)
+
+        if texture_count > 0 then
+            texture_map[i] = {
+                type  = "slider",
+                value = 1,
+                min   = 1,
+                max   = texture_count,
+            }
+        end
+    end
+
+    local current_drawable = GetPedDrawableVariation(ped, attribute.index)
+    local current_texture  = GetPedTextureVariation(ped, attribute.index)
+
+    return {
+        label        = attribute.label,
+        control_data = texture_map,
+        controls = {
+            {
+                type  = "index",
+                label = "Style",
+                value = current_drawable,
+                count = draw_count
+            },
+            {
+                type  = "slider",
+                label = "Variant",
+                value = current_texture,
+                min   = texture_map[current_texture].min,
+                max   = texture_map[current_texture].max
+            }
+        }
     }
 end
