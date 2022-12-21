9db898c67e0e982766407e7bb2ea9a9013c68dd0
open_mailbox <dev@open-mailbox.com>
2022-12-21 16:25:40 -0500

    Add more wardrobe locations.

--- src/wardrobe/client/models/locations.lua
+++ src/wardrobe/client/models/locations.lua
@@ -7,6 +7,16 @@ Locations = {
                 name     = "Binco Sinner Street",
                 location = vector3(427.276, -805.774, 28.591),
                 radius   = 7.5
+            },
+            {
+                name     = "Sub Urban Route 68",
+                location = vector3(617.554, 2764.363, 41.188),
+                radius   = 15.5
+            },
+            {
+                name     = "Binco Paleto",
+                location = vector3(6.095, 6511.412, 30.978),
+                radius   = 7.5
             }
         }
     },
@@ -18,6 +28,11 @@ Locations = {
                 name     = "Herr Kutz South Los Santos",
                 location = vector3(137.407, -1707.874, 28.392),
                 radius   = 3.5
+            },
+            {
+                name     = "Herr Kutz Paleto",
+                location = vector3(-278.533, 6228.525, 30.796),
+                radius     = 4.5
             }
         }
     }
