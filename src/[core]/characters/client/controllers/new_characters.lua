7c89b160083ce0f54ffbfd1ab224cad458827fab
open_mailbox <dev@open-mailbox.com>
2022-12-23 11:33:02 -0500

    Do a screen fade transition when initializing a new character.

--- src/[core]/characters/client/controllers/new_characters.lua
+++ src/[core]/characters/client/controllers/new_characters.lua
@@ -1,15 +1,24 @@
 local POSITION = vector3(-802.5900, 172.6235, 72.8446) -- Michael's house living room
-local HEADING  = 289.1680
+local HEADING  = 90.0
 
 local function create(_, cb)
     local ped     = PlayerPedId()
     local x, y, z = table.unpack(POSITION)
+
+    DoScreenFadeOut(1500)
+    repeat
+        Citizen.Wait(100)
+    until IsScreenFadedOut()
 
     SetEntityCollision(ped, true)
     FreezeEntityPosition(ped, false)
     SetEntityCoordsNoOffset(ped, x, y, z)
     SetEntityHeading(ped, HEADING)
     SetEntityVisible(ped, true)
+    SetNuiFocus(false, false)
+
+    Citizen.Wait(500)
+    DoScreenFadeIn(1500)
 
     TriggerEvent(Events.CREATE_WARDROBE_SESSION)
     cb({})
