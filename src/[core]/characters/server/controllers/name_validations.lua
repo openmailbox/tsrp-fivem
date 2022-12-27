67dde3f3a84465e3a178ec3daf11746d6ad430c9
open_mailbox <dev@open-mailbox.com>
2022-12-27 14:37:12 -0500

    Implement basic back-end name validation for new characters.

--- src/[core]/characters/server/controllers/name_validations.lua
+++ src/[core]/characters/server/controllers/name_validations.lua
@@ -1,12 +1,24 @@
 local function create(data)
-    local player_id = source
+    local first       = tostring(data.first)
+    local last        = tostring(data.last)
+    local player_id   = source
+    local valid_first = true
+    local valid_last  = true
 
-    -- TODO: Validate
+    if not data.first or string.len(first) < 3 then
+        valid_first = false
+    end
+
+    if not data.last or string.len(last) < 3 then
+        valid_last = false
+    end
+
+    -- TODO: Implement blacklisted words based on server.cfg convar
 
     TriggerClientEvent(Events.UPDATE_CHARACTER_NAME_VALIDATION, player_id, {
-        first   = true,
-        last    = true,
-        success = true
+        first   = valid_first,
+        last    = valid_last,
+        success = valid_first and valid_last
     })
 end
 RegisterNetEvent(Events.CREATE_CHARACTER_NAME_VALIDATION, create)
