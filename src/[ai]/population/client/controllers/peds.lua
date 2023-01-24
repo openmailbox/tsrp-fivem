348f0828ca8c2aa63c2dbf62bbc7d64d77d06f2e
open_mailbox <dev@open-mailbox.com>
2023-01-23 21:59:10 -0500

    Set individual combat attributes for spawned population cops.

--- src/[ai]/population/client/controllers/peds.lua
+++ src/[ai]/population/client/controllers/peds.lua
@@ -10,5 +10,9 @@ local function update(data)
     if data.combat_ability then
         SetPedCombatAbility(entity, data.combat_ability)
     end
+
+    for _, attrib in ipairs(data.combat_attribs or {}) do
+        SetPedCombatAttributes(entity, attrib, true)
+    end
 end
 RegisterNetEvent(Events.UPDATE_POPULATION_PED, update)
--- src/[ai]/population/server/controllers/entity_relgroups.lua
+++ src/[ai]/population/server/controllers/entity_relgroups.lua
@@ -3,15 +3,17 @@ local function update(data)
     if data.group == "COP" then
         GiveWeaponToPed(data.entity, GetHashKey("WEAPON_COMBATPISTOL"), 100, false, false)
         GiveWeaponToPed(data.entity, GetHashKey("WEAPON_NIGHTSTICK"), 0, false, false)
+
         SetPedArmour(data.entity, 75)
+
+        TriggerClientEvent(Events.UPDATE_POPULATION_PED, data.owner, {
+            net_id         = data.net_id,
+            driver_ability = 1.0,
+            combat_ability = 2,
+            combat_attribs = { 0, 1, 2, 3, 46, 52}
+        })
     elseif string.match(data.group, "AMBIENT_GANG") and math.random() < 0.33 then
         GiveWeaponToPed(data.entity, GetHashKey("WEAPON_PISTOL"), 100, false, false)
     end
-
-    TriggerClientEvent(Events.UPDATE_POPULATION_PED, data.owner, {
-        net_id         = data.net_id,
-        driver_ability = 1.0,
-        combat_ability = 2
-    })
 end
 AddEventHandler(Events.UPDATE_ENTITY_RELGROUP, update)
