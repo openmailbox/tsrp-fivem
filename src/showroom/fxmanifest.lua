26278960074af6a875eb968513b672212c465fc0
open_mailbox <dev@open-mailbox.com>
2023-03-06 09:20:22 -0500

    Add boilerplate for showroom resource.

---
+++ src/showroom/fxmanifest.lua
@@ -0,0 +1,27 @@
+---@diagnostic disable: undefined-global
+
+fx_version "cerulean"
+games { "gta5" }
+lua54 "yes"
+
+author "open_mailbox <dev@open-mailbox.com>"
+description "Provides the UI for the vehicle model browser."
+version "0.0.1"
+
+client_scripts {
+    "@common/shared/events.lua",
+    "client/**/*.lua"
+}
+
+server_scripts {
+    "@common/shared/events.lua",
+    "server/**/*.lua"
+}
+
+ui_page "web/dist/index.html"
+
+files {
+    "web/dist/**/*.html",
+    "web/dist/**/*.js",
+    "web/dist/**/*.css"
+}
