bf58754f57130cf3a66466d6a8b2360f488e9162
open_mailbox <dev@open-mailbox.com>
2023-03-14 09:55:11 -0400

    Add additional vehicle rental locations and spawn points.

--- src/rentals/client/models/rent_locations.lua
+++ src/rentals/client/models/rent_locations.lua
@@ -1,13 +1,62 @@
 RentLocations = {
     ["Bus Depot"] = {
         location   = vector3(434.3948, -654.2921, 28.7572),
+        spawns     = {
+            vector4(416.0175, -651.9574, 28.5003, 89.3267),
+            vector4(393.5267, -649.5768, 28.5003, 88.6606),
+            vector4(408.4593, -636.1716, 28.5001, 269.0850),
+            vector4(432.0027, -616.2574, 28.5000, 263.6984)
+        },
         categories = {
+            {
+                name   = "Cycles",
+                models = {
+                    { name = "fixter",  price = 25 },
+                    { name = "bmx",     price = 25 }
+                }
+            },
             {
                 name   = "Compacts",
                 models = {
                     { name = "blista", price = 50 },
                     { name = "club",   price = 50 },
                     { name = "kanjo",  price = 50 }
+                }
+            },
+            {
+                name   = "Sedans",
+                models = {
+                    { name = "asea",     price = 100 },
+                    { name = "intruder", price = 100 },
+                    { name = "primo",    price = 100 }
+                }
+            },
+            {
+                name   = "SUVs",
+                models = {
+                    { name = "baller", price = 150 },
+                    { name = "mesa",   price = 150 },
+                    { name = "xls",    price = 150 }
+                }
+            },
+        }
+    },
+
+    ["Airport"] = {
+        location   = vector3(-832.8307, -2351.1672, 14.5706),
+        spawns     = {
+            vector4(-812.1029, -2350.0952, 14.5706, 333.6624),
+            vector4(-830.0966, -2331.7759, 14.5706, 327.5406),
+            vector4(-825.3854, -2368.7280, 14.5707, 327.7874),
+            vector4(-801.8856, -2337.4504, 14.5706, 311.2072)
+        },
+        categories = {
+            {
+                name   = "Motorcycles",
+                models = {
+                    { name = "bati",    price = 150 },
+                    { name = "sanchez", price = 150 },
+                    { name = "avarus",  price = 150 }
                 }
             },
             {
@@ -37,46 +86,24 @@ RentLocations = {
         }
     },
 
-    ["Airport"] = {
-        location   = vector3(-832.8307, -2351.1672, 14.5706),
+    ["Vespucci Beach Bikes"] = {
+        location   = vector3(-1108.2147, -1693.6359, 4.3742),
         spawns     = {
-            vector4(-812.1029, -2350.0952, 14.5706, 333.6624),
-            vector4(-830.0966, -2331.7759, 14.5706, 327.5406)
+            vector4(-1114.8972, -1693.5851, 4.4190, 42.8307),
+            vector4(-1110.8212, -1699.3270, 4.4190, 33.3761),
+            vector4(-1105.5436, -1705.6290, 4.4190, 47.0040)
         },
         categories = {
             {
-                name   = "Compacts",
+                name   = "Cycles",
                 models = {
-                    { name = "blista", price = 50 },
-                    { name = "club",   price = 50 },
-                    { name = "kanjo",  price = 50 }
+                    { name = "fixter",   price = 25 },
+                    { name = "bmx",      price = 25 },
+                    { name = "tribike",  price = 25 },
+                    { name = "scorcher", price = 25 },
+                    { name = "cruiser",  price = 25 },
                 }
             },
-            {
-                name   = "Sedans",
-                models = {
-                    { name = "asea",     price = 100 },
-                    { name = "intruder", price = 100 },
-                    { name = "primo",    price = 100 }
-                }
-            },
-            {
-                name   = "SUVs",
-                models = {
-                    { name = "baller", price = 150 },
-                    { name = "mesa",   price = 150 },
-                    { name = "xls",    price = 150 }
-                }
-            },
-            {
-                name   = "Sports",
-                models = {
-                    { name = "massacro", price = 200 },
-                    { name = "banshee",  price = 200 },
-                    { name = "coquette", price = 200 }
-                }
-            },
-
         }
     }
 }
