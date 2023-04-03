cd59c99140ab5d79c24db84c09a48c3c41007704
open_mailbox <dev@open-mailbox.com>
2023-04-03 10:36:37 -0400

    Show/hide phone prop during phone session.

--- src/phone/client/phone_session.lua
+++ src/phone/client/phone_session.lua
@@ -1,5 +1,14 @@
 PhoneSession = {}
 
+-- Forward declarations
+local init_prop,
+      play_anim
+
+local PED_RIGHT_HAND = 28422 -- bone index
+local PHONE_MODEL    = "prop_amb_phone"
+
+local Animation = { DICTIONARY = "cellphone@" }
+
 local current_session = nil
 
 function PhoneSession.get_current()
@@ -17,11 +26,58 @@ end
 
 function PhoneSession:cleanup()
     current_session = nil
-    SetNuiFocus(false, false)
+
+    play_anim(Animation.DICTIONARY, "cellphone_text_out")
+
+
+    Citizen.SetTimeout(500, function()
+        ClearPedTasks(PlayerPedId())
+        DeleteEntity(self.prop)
+        SetNuiFocus(false, false)
+    end)
 end
 
 function PhoneSession:initialize()
     current_session = self
+
+    play_anim(Animation.DICTIONARY, "cellphone_text_in")
+
+    Citizen.SetTimeout(500, function()
+        self.prop = init_prop()
+    end)
+
     SendNUIMessage({ type = Events.CREATE_PHONE_SESSION })
     SetNuiFocus(true, true)
+end
+
+-- @local
+function init_prop()
+    local bone = GetPedBoneIndex(PlayerPedId(), PED_RIGHT_HAND)
+
+    if not HasModelLoaded(PHONE_MODEL) then
+        RequestModel(PHONE_MODEL)
+
+        repeat
+            Citizen.Wait(50)
+        until HasModelLoaded(PHONE_MODEL)
+    end
+
+    local prop = CreateObject(PHONE_MODEL, 1.0, 1.0, 1.0, true, false, false)
+
+    AttachEntityToEntity(prop, PlayerPedId(), bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
+
+    return prop
+end
+
+-- @local
+function play_anim(dictionary, anim)
+    if not HasAnimDictLoaded(dictionary) then
+        RequestAnimDict(dictionary)
+
+        repeat
+            Citizen.Wait(50)
+        until HasAnimDictLoaded(dictionary)
+    end
+
+    TaskPlayAnim(PlayerPedId(), dictionary, anim, -3.0, 3.0, -1, 50, 0, false, false, false)
 end
