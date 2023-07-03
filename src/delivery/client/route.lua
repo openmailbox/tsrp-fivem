12f44cc3f412f66de98ffdab1c2e8deb573d9a86
open_mailbox <dev@open-mailbox.com>
2023-07-03 16:12:23 -0400

    Implement keybind to attach package for delivery.

--- src/delivery/client/route.lua
+++ src/delivery/client/route.lua
@@ -1,9 +1,14 @@
 Route = {}
 
 -- Forward declarations
-local get_first_available
+local activate_package_search,
+      attach_package,
+      play_emote,
+      get_first_available,
+      start_updates
 
 local active_route = nil
+local is_polling   = false
 
 function Route.get_active()
     return active_route
@@ -12,6 +17,7 @@ end
 function Route.setup(depot)
     if active_route then
         active_route:cleanup()
+        active_route = nil
     end
 
     local route = Route:new({ depot = depot })
@@ -77,7 +83,7 @@ function Route:initialize()
         })
     end
 
-    Tutorial.initialize(self)
+    start_updates(self)
 end
 
 -- @local
@@ -102,4 +108,74 @@ function get_first_available(points)
     end
 
     return nil
+end
+
+-- @local
+function activate_package_search()
+    if is_polling then return end
+    is_polling = true
+
+    Citizen.CreateThread(function()
+        while is_polling do
+            if IsControlJustPressed(0, 47) then -- G
+                if GetEntitySpeed(PlayerPedId()) == 0.0 then
+                    is_polling = false
+                    attach_package()
+                else
+                    Tutorial.show_instructions()
+                end
+            end
+
+            Citizen.Wait(0)
+        end
+    end)
+end
+
+-- @local
+function attach_package()
+    TaskLeaveVehicle(PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), false), 256)
+
+    Citizen.CreateThread(function()
+        repeat
+            Citizen.Wait(100)
+        until not IsPedInAnyVehicle(PlayerPedId(), false)
+
+        play_emote()
+    end)
+end
+
+-- @local
+function play_emote()
+    -- Avoid making the emote resource a hard dependency so we can easily swap out for whatever emote provider.
+    if exports["rpemotes"] then
+        exports["rpemotes"]:EmoteCommandStart("box")
+    else
+        Logging.log(Logging.WARN, "Unable to attach box via emote.")
+    end
+end
+
+-- @local
+function start_updates(route)
+    Citizen.CreateThread(function()
+        local last_vehicle = 0
+
+        while Route.get_active() == route do
+            local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+
+            if vehicle > 0 and last_vehicle == 0 then -- when entering the vehicle
+                activate_package_search()
+                Tutorial.show_instructions()
+            elseif vehicle == 0 and last_vehicle > 0 then -- when leaving the vehicle
+                is_polling = false
+            end
+
+            if vehicle == 0 then
+                Tutorial.enter_vehicle()
+            end
+
+            last_vehicle = vehicle
+
+            Citizen.Wait(1000)
+        end
+    end)
 end
